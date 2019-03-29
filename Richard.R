ad3297c038673b4baf9423e40352b3a198dc7ac6
Richard Hart <6999074+rhart211@users.noreply.github.com>
2019-03-29 17:19:01 -0400

    Updated Richard.R

--- Richard.R
+++ Richard.R
@@ -1,1 +1,6 @@
 # Richard's R File
+require("mlbench")
+require("corrplot")
+data(PimaIndiansDiabetes2,package = "mlbench") #see note about correction to dataset at https://www.rdocumentation.org/packages/mlbench/versions/2.1-1/topics/PimaIndiansDiabetes
+Pima <- PimaIndiansDiabetes2;
+rm(PimaIndiansDiabetes2)
