bc4b2f2e8850d0b775c8ae12282c08b44d6528ec
Richard Hart <6999074+rhart211@users.noreply.github.com>
2019-03-29 20:15:30 -0400

    update R

--- Richard.R
+++ Richard.R
@@ -4,3 +4,6 @@ require("corrplot")
 data(PimaIndiansDiabetes2,package = "mlbench") #see note about correction to dataset at https://www.rdocumentation.org/packages/mlbench/versions/2.1-1/topics/PimaIndiansDiabetes
 Pima <- PimaIndiansDiabetes2;
 rm(PimaIndiansDiabetes2)
+
+%>% mutate(Attrition=ifelse(Attrition=="Yes", 1, 0))
+tempdf <- tempdf %>% mutate(diabetes=ifelse(diabetes=="pos", 1, 0))
